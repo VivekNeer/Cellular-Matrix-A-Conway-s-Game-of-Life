50288a1752dd5082818ff8dd143af50dbbb63968
Vivek Neeralagi <139262048+VivekNeer@users.noreply.github.com>
2024-10-15 01:03:49 +0530

    LottieFiles And Some Fixes for the Navbar

--- frontend/src/components/CustomNavbar.jsx
+++ frontend/src/components/CustomNavbar.jsx
@@ -1,28 +1,20 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import { TfiAlignJustify } from "react-icons/tfi";
-import { MdInvertColors } from "react-icons/md";
+import Rubiks from "../assets/squares.json";
+import { Player } from "@lottiefiles/react-lottie-player";
+import { useEffect } from "react";
+import { themeChange } from "theme-change";
 
 const CustomNavbar = () => {
+  useEffect(() => {
+    themeChange(false);
+  }, []);
+
   return (
     <div className="navbar bg-base-100">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            {/* <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg> */}
             <TfiAlignJustify />
           </div>
           <ul
@@ -30,52 +22,72 @@ const CustomNavbar = () => {
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
           >
             <li>
-            <Link to="/">Home</Link>
+              <Link to="/">Home </Link>
             </li>
             <li>
-            <Link to="/gallery">Gallery</Link>
-              <ul className="p-2">
-                <li>
-                <Link to="/gallery">Endless</Link>
-                </li>
-                <li>
-                <Link to="/gallery">Endless</Link>
-                </li>
-              </ul>
-            </li>
-            <li>
-            <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </div>
-        <a className="btn btn-ghost text-xl">CellularMatrix</a>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Home</a>
-          </li>
-          <li>
-            <details>
-              <summary><Link to="/gallery">Gallery</Link></summary>
+              <Link to="/gallery">Gallery</Link>
               <ul className="p-2">
                 <li>
                   <Link to="/gallery">Endless</Link>
                 </li>
                 <li>
-                <Link to="/gallery">Space Craft</Link>
+                  <Link to="/gallery">Endless</Link>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            {/* Add the theme select button inside the dropdown for small screens */}
+            <li>
+              <select data-choose-theme className="btn w-full">
+                <option value="luxury">Luxury</option>
+                <option value="cupcake">Cupcake</option>
+                <option value="cyberpunk">Cyberpunk</option>
+              </select>
+            </li>
+          </ul>
+        </div>
+        <Link to="/" className="btn btn-ghost text-xl">
+          CellularMatrix
+          <Player autoplay loop src={Rubiks} className="h-12 w-12" />
+        </Link>
+      </div>
+
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <details>
+              <summary>
+                <Link to="/gallery">Gallery</Link>
+              </summary>
+              <ul className="p-2">
+                <li>
+                  <Link to="/gallery">Endless</Link>
+                </li>
+                <li>
+                  <Link to="/gallery">Space Craft</Link>
                 </li>
               </ul>
             </details>
           </li>
           <li>
-          <Link to="/about">About</Link>
+            <Link to="/about">About</Link>
           </li>
         </ul>
       </div>
+
       <div className="navbar-end">
         <a className="btn mr-3">Login/Signup</a>
-        <a className="btn"><MdInvertColors /></a>
+        {/* Show the theme select button only on large screens */}
+        <select data-choose-theme className="btn hidden lg:block">
+          <option value="luxury">Luxury</option>
+          <option value="cupcake">Cupcake</option>
+          <option value="cyberpunk">Cyberpunk</option>
+        </select>
       </div>
     </div>
   );
